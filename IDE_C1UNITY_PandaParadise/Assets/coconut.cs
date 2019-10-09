dcb0316e681d768c977ddc53632d3e9f36ffc26c
AlexandreGomy <56352534+AlexandreGomy@users.noreply.github.com>
2019-10-09 15:27:28 +0200

    initial commit
    Initial commit

---
+++ IDE_C1UNITY_PandaParadise/Assets/coconut.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coconut : MonoBehaviour
+{
+
+    public float degats = 30;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
